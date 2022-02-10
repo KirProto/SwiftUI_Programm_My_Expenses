3220fcbbd66094c1fdc09bf218e274ea7d5eda04
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 12:15:53 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Programm_My_Expenses/Yan_SwiftUI_Programm_My_ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Programm_My_ExpensesApp.swift
+//  Yan_SwiftUI_Programm_My_Expenses
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Programm_My_ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
